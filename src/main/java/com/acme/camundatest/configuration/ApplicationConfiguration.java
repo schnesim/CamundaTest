e7c92f0ecf0eef3bb00fc193886672bfe40d5952
Simon Schneider <extern.schneider_simon@allianz.de>
2020-08-17 10:52:05 +0200

    playing with database connection

--- src/main/java/com/acme/camundatest/configuration/ApplicationConfiguration.java
+++ src/main/java/com/acme/camundatest/configuration/ApplicationConfiguration.java
@@ -13,6 +13,7 @@ public class ApplicationConfiguration {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
         dataSource.setUrl("jdbc:h2:mem:process-engine;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+//        dataSource.setUrl("jdbc:h2:mem:Angebote;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
         dataSource.setUsername("sa");
         dataSource.setPassword("");
         return dataSource;
--- src/main/java/com/acme/camundatest/endpoint/CamundaTestEndpoint.java
+++ src/main/java/com/acme/camundatest/endpoint/CamundaTestEndpoint.java
@@ -1,7 +1,10 @@
 package com.acme.camundatest.endpoint;
 
 import java.util.Optional;
+import org.camunda.bpm.engine.HistoryService;
 import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.history.HistoricProcessInstance;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +18,23 @@ public class CamundaTestEndpoint {
     @Autowired
     private RuntimeService runtimeService;
     
+    @Autowired
+    private HistoryService historyService;
+    
     @RequestMapping(value = {"/save", "/save/{way}"})
-    public String startProcess(@PathVariable Optional<String> way) {
+    public String startProcess(@PathVariable Optional<String> way) throws InterruptedException {
         VariableMap variables = Variables.createVariables();
         variables.put("task", way.isPresent() ? way.get() : "");
-        runtimeService.startProcessInstanceByKey("Process_0ts9hy7", variables);
+        String businessKey = "someKey";
+        int size = historyService.createHistoricProcessInstanceQuery().processInstanceBusinessKey(businessKey).list().size();
+        ProcessInstance singleResult = runtimeService.createProcessInstanceQuery().processInstanceBusinessKey(businessKey).singleResult();
+        runtimeService.startProcessInstanceByKey("Process_0ts9hy7", businessKey, variables);
+        Thread.sleep(1000);
+        ProcessInstance singleResult2 = runtimeService.createProcessInstanceQuery().processInstanceBusinessKey(businessKey).singleResult();
+        System.out.println(singleResult);
+        System.out.println(singleResult2);
+        int newSize = historyService.createHistoricProcessInstanceQuery().processInstanceBusinessKey(businessKey).list().size();
+        System.out.println(size + " " + newSize);
         return "good job";
     }
 }
