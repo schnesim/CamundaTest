3aa87a5b59092845bc2edba35f61e37f8857f198
Simon Schneider <extern.schneider_simon@allianz.de>
2020-08-17 11:25:58 +0200

    with the right dependency, Spring is picking up database settings in the properties file

--- src/main/java/com/acme/camundatest/configuration/ApplicationConfiguration.java
+++ src/main/java/com/acme/camundatest/configuration/ApplicationConfiguration.java
@@ -1,21 +1,21 @@
-package com.acme.camundatest.configuration;
-
-import javax.sql.DataSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-@Configuration
-public class ApplicationConfiguration {
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:process-engine;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
-//        dataSource.setUrl("jdbc:h2:mem:Angebote;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");
-        return dataSource;
-    }
-}
+//package com.acme.camundatest.configuration;
+//
+//import javax.sql.DataSource;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//
+//@Configuration
+//public class ApplicationConfiguration {
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.h2.Driver");
+//        dataSource.setUrl("jdbc:h2:mem:process-engine;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+////        dataSource.setUrl("jdbc:h2:mem:Angebote;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+//        dataSource.setUsername("sa");
+//        dataSource.setPassword("");
+//        return dataSource;
+//    }
+//}
